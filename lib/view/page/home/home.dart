d263ac146e0fe535ac34a044ae518f96f02e5dc2
Yuga <yugaKeio23@gmail.com>
2023-08-30 13:11:45 +0900

    home page

--- lib/view/page/home/home.dart
+++ lib/view/page/home/home.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_svg/flutter_svg.dart';
+
+import 'package:yagamy/gen/assets.gen.dart';
+
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -20,16 +24,43 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return const CustomScrollView(
-      physics: ScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
+    return CustomScrollView(
+      physics: const ScrollPhysics(parent: NeverScrollableScrollPhysics()),
       slivers: <Widget>[
         SliverAppBar(
-          title: Text('Yagamy'),
-          actions: <Widget>[
+          title: SvgPicture.asset(
+            Assets.logo.yagamiFestival2023.path,
+            width: 100,
+            height: 40,
+          ),
+          actions: const <Widget>[
             Icon(Icons.account_circle_outlined),
             SizedBox(width: 15),
           ],
-        )
+          centerTitle: false,
+        ),
+        SliverList(
+          delegate: SliverChildListDelegate(
+            [
+              Card(
+                elevation: 0.0,
+                shape: RoundedRectangleBorder(
+                  side:
+                      BorderSide(color: Theme.of(context).colorScheme.outline),
+                  borderRadius: const BorderRadius.all(Radius.circular(8)),
+                ),
+                margin: const EdgeInsets.only(
+                    left: 10, top: 5, right: 10, bottom: 10),
+                child: const Column(
+                  children: [
+                    Text('第24回矢上祭'),
+                    Text('2023年9月23日(土) 10:00-'),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ],
     );
   }
