a4175ebb8bd0afed30c9f51c0a98cd012749b44d
Yuga <yugaKeio23@gmail.com>
2023-09-02 16:40:57 +0900

    add firebase core

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 
 import 'package:yagamy/constant/theme/theme.dart';
@@ -8,6 +9,8 @@ import 'package:yagamy/router/routes.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
 
   final messaging = FirebaseMessaging.instance;
   await messaging.requestPermission();
