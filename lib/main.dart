ecb5c2272f85ae0a564e6ec22eb0e4503a299d8b
Yuga <yugaKeio23@gmail.com>
2023-09-02 09:21:54 +0900

    edit main function

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
 import 'package:yagamy/constant/theme/theme.dart';
 import 'package:yagamy/router/routes.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  final messaging = FirebaseMessaging.instance;
+  await messaging.requestPermission();
+  final token = await messaging.getToken();
+  print('FCM Token: $token');
+
   runApp(const ProviderScope(child: MyApp()));
 }
 
